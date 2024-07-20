cbd3bc355ffe05e8e91b3f7e3c616bb1d59f6dc4
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-07-21 02:42:58 +0530

    KAN-2 change the response entity type

--- src/main/java/lk/purna/healthy_life/controller/FoodDetailsController.java
+++ src/main/java/lk/purna/healthy_life/controller/FoodDetailsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 
 
 @RestController
@@ -27,6 +28,8 @@ public class FoodDetailsController {
         FoodDetailsDto foodDetailsDto = modelMapper.map(foodDetailsRq, FoodDetailsDto.class);
         FoodDetailsResponse foodDetailsResponse = foodDetailsService.createFoodDetails(foodDetailsDto);
 
-        return new ResponseEntity<>(foodDetailsResponse, HttpStatus.CREATED);
+//        return new ResponseEntity<>(foodDetailsResponse, HttpStatus.CREATED);
+
+        return ResponseEntity.created(URI.create("/food_details")).body(foodDetailsResponse);
     }
 }
