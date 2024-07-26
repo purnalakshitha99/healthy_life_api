ddb2c6913ae2c4b81b4d4606c6613be0f7671849
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-07-26 16:51:07 +0530

    KAN-8 get all activity levels

--- src/main/java/lk/purna/healthy_life/controller/ActivityLevelController.java
+++ src/main/java/lk/purna/healthy_life/controller/ActivityLevelController.java
@@ -3,13 +3,20 @@ package lk.purna.healthy_life.controller;
 import lk.purna.healthy_life.controller.dto.ActivityLevelDto;
 import lk.purna.healthy_life.controller.request.ActivityLevelRq;
 import lk.purna.healthy_life.controller.response.ActivityLevelResponse;
+import lk.purna.healthy_life.exception.ActivityLevelNotFoundException;
 import lk.purna.healthy_life.service.ActivityLevelService;
 import lombok.AllArgsConstructor;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -24,5 +31,13 @@ public class ActivityLevelController {
         ActivityLevelDto activityLevelDto = modelMapper.map(activityLevelRq, ActivityLevelDto.class);
 
         return activityLevelService.createActivityLevel(activityLevelDto);
+    }
+
+    @GetMapping("/activity_levels")
+    public List<ResponseEntity<List<ActivityLevelResponse>>> getAllActivityLevel()throws ActivityLevelNotFoundException {
+
+        List<ActivityLevelResponse> activityLevelResponseList = activityLevelService.getAllActivityLevel();
+
+        return Collections.singletonList(new ResponseEntity<>(activityLevelResponseList,HttpStatus.FOUND));
     }
 }
--- src/main/java/lk/purna/healthy_life/service/impl/ActivityLevelServiceImpl.java
+++ src/main/java/lk/purna/healthy_life/service/impl/ActivityLevelServiceImpl.java
@@ -3,12 +3,15 @@ package lk.purna.healthy_life.service.impl;
 
 import lk.purna.healthy_life.controller.dto.ActivityLevelDto;
 import lk.purna.healthy_life.controller.response.ActivityLevelResponse;
+import lk.purna.healthy_life.exception.ActivityLevelNotFoundException;
 import lk.purna.healthy_life.model.ActivityLevel;
 import lk.purna.healthy_life.repository.ActivityLevelRepository;
 import lk.purna.healthy_life.service.ActivityLevelService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -25,5 +28,16 @@ public class ActivityLevelServiceImpl implements ActivityLevelService {
         activityLevelRepository.save(activityLevel);
 
         return modelMapper.map(activityLevel, ActivityLevelResponse.class);
+    }
+
+    public List<ActivityLevelResponse> getAllActivityLevel()throws ActivityLevelNotFoundException {
+
+        List<ActivityLevel> activityLevelList  = activityLevelRepository.findAll();
+
+        if (activityLevelList.isEmpty()){
+            throw new ActivityLevelNotFoundException("activity levels empty");
+        }
+
+        return activityLevelList.stream().map(activityLevel -> modelMapper.map(activityLevel,ActivityLevelResponse.class)).toList();
     }
 }
