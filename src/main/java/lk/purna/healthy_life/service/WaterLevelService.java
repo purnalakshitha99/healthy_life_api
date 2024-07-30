a9eea7db8b86ef1e3b9f7f11380971456487fca1
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-07-30 11:22:36 +0530

    KAN-12 daily water intake successfully

--- src/main/java/lk/purna/healthy_life/service/WaterLevelService.java
+++ src/main/java/lk/purna/healthy_life/service/WaterLevelService.java
@@ -1,9 +1,11 @@
 package lk.purna.healthy_life.service;
 
 import lk.purna.healthy_life.controller.dto.WaterLevelDto;
+import lk.purna.healthy_life.controller.response.DailyWaterIntakeResponse;
 import lk.purna.healthy_life.controller.response.WaterLevelResponse;
 import lk.purna.healthy_life.exception.UserNotFoundException;
 import lk.purna.healthy_life.exception.WaterLevelNotFoundException;
+import lk.purna.healthy_life.exception.WeightLevelNotFoundException;
 
 import java.util.List;
 
@@ -11,4 +13,6 @@ public interface WaterLevelService {
     WaterLevelResponse createWaterLevelForUser(Long userId, WaterLevelDto waterLevelDto)throws UserNotFoundException;
 
     List<WaterLevelResponse> getSpecificUserWaterLevels(Long userId)throws UserNotFoundException, WaterLevelNotFoundException;
+
+    DailyWaterIntakeResponse getDailyWaterIntakeByUser(Long userId)throws UserNotFoundException, WeightLevelNotFoundException;
 }
--- src/main/java/lk/purna/healthy_life/service/impl/WaterLevelServiceImpl.java
+++ src/main/java/lk/purna/healthy_life/service/impl/WaterLevelServiceImpl.java
@@ -1,14 +1,19 @@
 package lk.purna.healthy_life.service.impl;
 
 import lk.purna.healthy_life.controller.dto.WaterLevelDto;
+import lk.purna.healthy_life.controller.response.DailyWaterIntakeResponse;
 import lk.purna.healthy_life.controller.response.WaterLevelResponse;
 import lk.purna.healthy_life.exception.UserNotFoundException;
 import lk.purna.healthy_life.exception.WaterLevelNotFoundException;
+import lk.purna.healthy_life.exception.WeightLevelNotFoundException;
+import lk.purna.healthy_life.model.Answer;
+import lk.purna.healthy_life.model.Gender;
 import lk.purna.healthy_life.model.User;
 import lk.purna.healthy_life.model.WaterLevel;
 import lk.purna.healthy_life.repository.UserRepository;
 import lk.purna.healthy_life.repository.WaterLevelRepository;
 import lk.purna.healthy_life.service.WaterLevelService;
+import lk.purna.healthy_life.service.WeightLevelService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -24,6 +29,7 @@ public class WaterLevelServiceImpl implements WaterLevelService {
     private final UserRepository userRepository;
     private final WaterLevelRepository waterLevelRepository;
     private ModelMapper modelMapper;
+    private WeightLevelService weightLevelService;
 
     @Override
     public WaterLevelResponse createWaterLevelForUser(Long userId, WaterLevelDto waterLevelDto) throws UserNotFoundException {
@@ -58,6 +64,48 @@ public class WaterLevelServiceImpl implements WaterLevelService {
 
       return   waterLevelList.stream().map(waterLevel -> modelMapper.map(waterLevel, WaterLevelResponse.class)).toList();
 
+
+    }
+
+    @Override
+    public DailyWaterIntakeResponse getDailyWaterIntakeByUser(Long userId) throws UserNotFoundException, WeightLevelNotFoundException {
+
+        User user = userRepository.findById(userId).orElseThrow(
+                ()-> new UserNotFoundException("That user Not in a db ")
+        );
+
+        Answer answers = user.getAnswer();
+
+        Float weight = weightLevelService.getLatestWeightByUserId(userId);
+        Integer age = answers.getAge();
+        Gender gender = answers.getGender();
+        Float waterIntake = 0.0F;
+
+        if(age < 18 && gender == Gender.MALE){
+            waterIntake = (float) ((weight * 0.06) + 0.5);
+        } else if (age < 18 && gender == Gender.FEMALE) {
+            waterIntake = (float) ((weight * 0.06) + 0.4);
+        } else if (age > 18 && age <= 30 && gender == Gender.MALE) {
+            waterIntake = (float) ((weight * 0.03) + 0.5);
+        } else if (age > 18 && age <= 30 && gender == Gender.FEMALE) {
+            waterIntake = (float) ((weight * 0.03) + 0.4);
+        } else if (age > 30 && age <= 50 && gender == Gender.MALE) {
+            waterIntake = (float) ((weight * 0.025) + 0.5);
+        } else if (age > 30 && age <= 50 && gender == Gender.FEMALE) {
+            waterIntake = (float) ((weight * 0.025) + 0.4);
+        } else if (age > 50 && gender == Gender.MALE) {
+            waterIntake = (float) ((weight * 0.02) + 0.5);
+        } else if (age > 50 && gender == Gender.FEMALE) {
+            waterIntake = (float) ((weight * 0.02) + 0.4);
+        }else {
+            return null;
+        }
+
+        DailyWaterIntakeResponse dailyWaterIntakeResponse = new DailyWaterIntakeResponse();
+
+        dailyWaterIntakeResponse.setDailyWaterIntake(waterIntake);
+
+        return dailyWaterIntakeResponse;
 
     }
 }
